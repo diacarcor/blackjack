3b70291be768cf33c31bc433053db664d98c04a9
diacarcor <>
2021-05-19 14:42:47 +0000

    Final version

--- main.py
+++ main.py
@@ -36,44 +36,6 @@ def compare_scores(player_score, computer_score):
     else:
         return "You lose. 😭"
           
-def finish(player, computer):
-    print(f"  Your final hand: {player['hand']}, final score: {player['score']}")
-
-    print(f"  Computer's final hand: {computer['hand']}, final score: {computer['score']}")
-
-def validate_scores (player_hand,computer_hand):
-    
-    player_score = calculate_score(player_hand)
-
-    computer_score = calculate_score(computer_hand)
-
-    print(f"  Your cards: {player_hand}, current score: {player_score}")
-
-    print(f"  Computer's first card:  {computer_hand[0]}")
-
-    if computer_score == 0 or player_score == 0 or player_score > 21 :
-        print(f"  Your final hand: {player_hand}, final score: {player_score}")
-
-        print(f"  Computer's final hand: {computer_hand}, final score: {computer_score}")
-
-        print(compare_scores(player_score, computer_score))
-    else:
-        another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-
-        if another_card == "y":
-            player_hand.append(deal_card())
-            validate_scores(player_hand, computer_hand)
-        else:
-            while computer_score < 17:
-                computer_hand.append(deal_card())
-                computer_score = calculate_score(computer_hand)
-
-            print(f"  Your final hand: {player_hand}, final score: {player_score}")
-
-            print(f"  Computer's final hand: {computer_hand}, final score: {computer_score}")
-
-            print(compare_scores(player_score, computer_score))
-            
 def start_game():
     """Creates the initial hands for player and computer"""
     clear()
@@ -85,82 +47,39 @@ def start_game():
         player_hand.append(deal_card())
         computer_hand.append(deal_card())
 
-    validate_scores(player_hand,computer_hand)
+    game_over = False
+
+    while not game_over:
+
+        #Get player an computer score
+        player_score = calculate_score(player_hand)
+
+        computer_score = calculate_score(computer_hand)
+
+        print(f"  Your cards: {player_hand}, current score: {player_score}")
+
+        print(f"  Computer's first card:  {computer_hand[0]}")
+
+        if computer_score == 0 or player_score == 0 or player_score > 21 :
+            game_over = True
+        else:
+            another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+
+            if another_card == "y":
+                player_hand.append(deal_card())
+            else:
+                game_over = True
+
+    while computer_score < 17:
+        computer_hand.append(deal_card())
+        computer_score = calculate_score(computer_hand)
+
+    print(f"  Your final hand: {player_hand}, final score: {player_score}")
+
+    print(f"  Computer's final hand: {computer_hand}, final score: {computer_score}")
+
+    print(compare_scores(player_score, computer_score))
 
 
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower() == "y":
     start_game()
-
-
-
-# def black_jack_validation(player_hand,computer_hand):
-#     """Validate if there is a black jack. Return 0 if computer wins. Return 1 if player wins. Return 2 if nobody has blackjack """
-#     if 10 in computer_hand and 11 in computer_hand:
-#         print("Computer Wins") 
-#         return True
-#     elif 10 in player_hand and 11 in player_hand:
-#         print("Player Wins")
-#         return True
-#     else:
-#         return False
-
-# def validate_score(score, hand):
-#     if score > 21:
-#         for card in hand:
-#             if card == 11:
-#                 score = score -10
-#                 if score > 21:
-#                     print("Player lose")
-#             else:
-#                 return score
-#     else:
-#         return score
-
-# def initial_hands():
-    
-    
-#     player_score = 0
-#     computer_score = 0
-
-#     is_black_jack = black_jack_validation( computer_hand,player_hand)
-
-#     if not is_black_jack:
-        
-#         player_score = player_hand[0] + player_hand[1]
-
-#         computer_score = computer_hand[0] + computer_hand[1]
-
-#         validate_score(player_score, player_hand)
-
-#         print(f"Your cards: {player_hand}, current score: {player_score}")
-
-#         print(f"Computer cards: {computer_hand}, current score: {computer_score}")   
-
-        
-
-
-
-
-
-
-
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-
