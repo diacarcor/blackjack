80f30acbfc656fdf473bbc58e86dc7848682eb39
diacarcor <>
2021-05-20 19:45:57 +0000

    Recursively

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from replit import clear
 from art import logo
 import random
 
-def deal_card():    
+
+def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
+
 def calculate_score(hand):
     """Take a hand and calculates the score in it"""
     score = sum(hand)
-    #To check if ther is a Blackjack
-    if len(hand)== 2 and score == 21:
+    # To check if ther is a Blackjack
+    if len(hand) == 2 and score == 21:
         return 0
-    #To check if there is an AS
+    # To check if there is an AS
     if score > 21 and 11 in hand:
-            index = hand.index(11)
-            hand[index] -= 10
-            score -= 10
+        index = hand.index(11)
+        hand[index] -= 10
+        score -= 10
     return score
+
 
 def compare_scores(player_score, computer_score):
     if player_score == computer_score:
@@ -35,7 +38,22 @@ def compare_scores(player_score, computer_score):
         return "You win. 😃"
     else:
         return "You lose. 😭"
-          
+
+
+def to_bet(player_hand, computer_hand, player_score):
+    print(f"  Your cards: {player_hand}, current score: {player_score}")
+    print(f"  Computer's first card:  {computer_hand[0]}")
+
+    another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    if another_card == "y":
+        player_hand.append(deal_card())
+        player_score = calculate_score(player_hand)
+        if player_score < 21:
+            to_bet(player_hand, computer_hand, player_score)
+
+    return player_hand
+
+
 def start_game():
     """Creates the initial hands for player and computer"""
     clear()
@@ -47,32 +65,16 @@ def start_game():
         player_hand.append(deal_card())
         computer_hand.append(deal_card())
 
-    game_over = False
+    # Get player an computer score
+    player_score = calculate_score(player_hand)
+    computer_score = calculate_score(computer_hand)
 
-    while not game_over:
-
-        #Get player an computer score
+    if computer_score != 0 and player_score != 0:
+        player_hand = to_bet(player_hand, computer_hand, player_score)
         player_score = calculate_score(player_hand)
-
-        computer_score = calculate_score(computer_hand)
-
-        print(f"  Your cards: {player_hand}, current score: {player_score}")
-
-        print(f"  Computer's first card:  {computer_hand[0]}")
-
-        if computer_score == 0 or player_score == 0 or player_score > 21 :
-            game_over = True
-        else:
-            another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-
-            if another_card == "y":
-                player_hand.append(deal_card())
-            else:
-                game_over = True
-
-    while computer_score < 17:
-        computer_hand.append(deal_card())
-        computer_score = calculate_score(computer_hand)
+        while computer_score < 17 and player_score < 21:
+            computer_hand.append(deal_card())
+            computer_score = calculate_score(computer_hand)
 
     print(f"  Your final hand: {player_hand}, final score: {player_score}")
 
